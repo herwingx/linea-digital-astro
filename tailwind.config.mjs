d4ab6eb099d403e6ffd113d184737c9716e5e854
Eduardo Macias <herwinx@proton.me>
2025-11-17 06:11:11 +0000

    Initialize project with essential configuration files and .gitignore

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: '#0F172A',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
